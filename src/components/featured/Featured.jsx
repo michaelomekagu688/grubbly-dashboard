57919f8b1c3905c4e0ebca5fc0cafa839ccc66ec
Omekagu Michael <michaelomekagu@gmail.com>
2025-07-01 21:33:33 +0100

    worked on the featured section

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import './featured.scss'
-import { MoreVert } from "@mui/icons-material";
+import { KeyboardArrowDown, KeyboardArrowUp, MoreVert } from "@mui/icons-material";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 
@@ -9,12 +9,40 @@ function Featured(){
     return(
         <div className="featured-cont">
             <div className="top">
-                <div> Total Revenue </div>
+                <div className="top-text"> Total Revenue </div>
                 <MoreVert />
             </div>
             <div className="bottom">
                 <div className="featuredChart"> 
                     <CircularProgressbar value={70} text="70%" strokeWidth={6} />   
+                </div>
+                <p className="total-text"> Total sales made today </p>
+                <span> $420 </span>
+                <p> Previous transaction processing. Last payments may not be concluded</p>
+                <div className="target-cont"> 
+                    <div className="targets-div">
+                        <div className="targets-tp"> Target </div>
+                        <div className="targets-dwn negative"> 
+                            <KeyboardArrowUp />
+                            <div> $12.4k </div>
+                        </div>
+                    </div>
+                    <div className="targets-div">
+                        <div className="targets-tp"> Target </div>
+                        <div className="targets-dwn positive"> 
+                            <KeyboardArrowDown />
+                            <div> $12.4k </div>
+                        </div>
+                    </div>
+                    <div className="targets-div">
+                        <div className="targets-tp"> Target </div>
+                        <div className="targets-dwn positive"> 
+                            <KeyboardArrowUp />
+                            <div> $12.4k </div>
+                        </div>
+                    </div>
+                   
+
                 </div>
 
             </div>
