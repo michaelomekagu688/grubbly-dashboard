7455e8c6f97cf86b1e8020ee2928c2ec7e03eedd
Omekagu Michael <michaelomekagu@gmail.com>
2025-07-04 00:47:22 +0100

    Created and is almost done with the single's page

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -1,9 +1,58 @@
 import React from "react";
 import './single.scss'
+import Sidebar from '../../components/sidebar/Sidebar'
+import Navbar from "../../components/navbar/Navbar";
+import { AddAPhoto } from "@mui/icons-material";
 
 function Single(){
     return(
-        <div> Single </div>
+        <div className="single"> 
+            <div className="single-cont">
+                <Sidebar />
+                <div className="single-wrapper">
+                    <Navbar />
+                    <div className="single-sec">
+                        <div className="top"> Add New User </div>
+                        <div className="bottom">  
+                            <div className="left">
+                                <AddAPhoto className="photo-icon" />
+                            </div>
+                            <div className="right">
+                                <div className="inner-right">
+                                    <label> Name and Surname</label>
+                                    <input type="text" placeholder="John Doe" />
+                                </div>
+                                <div className="inner-right">
+                                    <label> Name and Surname</label>
+                                    <input type="text" placeholder="John Doe" />
+                                </div>
+                                <div className="inner-right">
+                                    <label> Name and Surname</label>
+                                    <input type="text" placeholder="John Doe" />
+                                </div>
+                                <div className="inner-right">
+                                    <label> Name and Surname</label>
+                                    <input type="text" placeholder="John Doe" />
+                                </div>
+                                <div className="inner-right">
+                                    <label> Name and Surname</label>
+                                    <input type="text" placeholder="John Doe" />
+                                </div>
+                                <div className="inner-right">
+                                    <label> Name and Surname</label>
+                                    <input type="text" placeholder="John Doe" />
+                                </div>
+                                <div className="inner-right">
+                                    <label> Name and Surname</label>
+                                    <input type="text" placeholder="John Doe" />
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
