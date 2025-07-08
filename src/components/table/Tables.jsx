b817c1e69b3d29416df11e9fe50f784bfbd14c76
Omekagu Michael <michaelomekagu@gmail.com>
2025-07-08 10:52:23 +0100

    made sidebar responsive

--- src/components/table/Tables.jsx
+++ src/components/table/Tables.jsx
@@ -19,34 +19,32 @@ function Tables(){
       name,
       calories,
       fat,
-      carbs,
       protein,
       methods,
       status,
     ) {
-      return { name, calories, fat, carbs, protein, methods, status };
+      return { name, calories, fat, protein, methods, status };
     }
 
 
         
     const rows = [
-    createData(1143155, 'Acer Nitro', 'John smith', '1 March', 785,'Cash on Delivery', 'Approved'),
-    createData(2235235, 'Play Station 5', 'Michael Doe', '1 March', 900,'Online Payment', 'Pending'),
-    createData(2342353, 'Redragon S101', 'John Smith', '1 March', 35,'Online Payment', 'Pending'),
-    createData(2357741, 'Razor Blade 15', 'Jane Smith', '1 March', 920,'Online', 'Approved'),
-    createData(2342355, 'ASUS ROG Strike', 'Harold Carol', '1 March', 2000,'Online', 'Pending'),
+    createData(1143155, 'Acer Nitro', 'John smith', 785,'Cash on Delivery', 'Approved'),
+    createData(2235235, 'Play Station 5', 'Michael Doe', 900,'Online Payment', 'Pending'),
+    createData(2342353, 'Redragon S101', 'John Smith', 35,'Online Payment', 'Pending'),
+    createData(2357741, 'Razor Blade 15', 'Jane Smith', 920,'Online', 'Approved'),
+    createData(2342355, 'ASUS ROG Strike', 'Harold Carol', 2000,'Online', 'Pending'),
     ];
 
     return(
         <div className="table">
               <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <Table sx={{ minWidth: 300 }} aria-label="simple table">
                     <TableHead>
                         <TableRow>
                             <TableCell>Tracking ID </TableCell>
                             <TableCell >Produc </TableCell>
                             <TableCell >Customer </TableCell>
-                            <TableCell > Date </TableCell>
                             <TableCell > Amount </TableCell>
                             <TableCell > Payment Method </TableCell>
                             <TableCell > Status </TableCell>
@@ -63,7 +61,6 @@ function Tables(){
                         </TableCell>
                         <TableCell>{row.calories}</TableCell>
                         <TableCell>{row.fat}</TableCell>
-                        <TableCell>{row.carbs}</TableCell>
                         <TableCell>{row.protein}</TableCell>
                         <TableCell>{row.methods}</TableCell>
                         <TableCell>{row.status}</TableCell>
